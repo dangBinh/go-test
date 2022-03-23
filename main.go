1ecf2869da6b701316cad807916a5323b8f41077
Binh Quach <binhqd.fami@gmail.com>
2022-03-23 17:44:46 +0700

    Add averageNumber function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 // The difficulty of the function: O(n)
@@ -15,6 +16,26 @@ func testValidity(text string) bool {
 	return true
 }
 
+// The difficulty of the function: O(n)
+// averageNumber calculates the average number from all the numbers
+func averageNumber(text string) (avgNumber float32) {
+	re := regexp.MustCompile(`[0-9]+`)
+	matches := re.FindAllString(text, -1)
+	length := len(matches)
+
+	if length == 0 {
+		return 0
+	}
+	var total float32
+	for _, match := range matches {
+		fmt.Println(match)
+		number, _ := strconv.ParseFloat(match, 10)
+		total += float32(number)
+	}
+	return total / float32(length)
+}
+
 func main() {
 	fmt.Println(testValidity("23-ab-48-caba-56-abc"))
+	fmt.Println(averageNumber("23-ab-48-caba-56-abc"))
 }
