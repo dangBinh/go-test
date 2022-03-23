03211a0d20e6a58a7f57e74fc17a15974b40b3e8
Binh Quach <binhqd.fami@gmail.com>
2022-03-23 17:31:57 +0700

    Add testValidity function

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
+// The difficulty of the function: O(n)
+// testValidity checks whether text complies with the format.
+func testValidity(text string) bool {
+	regex := `^(([0-9]+[-][a-z]+)(?:[-]*)?)+$`
+	if m, _ := regexp.MatchString(regex, text); !m {
+		return false
+	}
+	return true
+}
+
 func main() {
-	// Test 1:
+	fmt.Println(testValidity("23-ab-48-caba-56-abc"))
 }
